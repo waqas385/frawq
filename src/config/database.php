a83664f10b47b607a1653c394b7f9b272ead2633
Waqas Ahmed <waqas385@gmail.com>
2015-12-08 11:39:07 +0500

    Update database info

--- src/config/database.php
+++ src/config/database.php
@@ -4,12 +4,22 @@ namespace Config;
 class DBSettings{
     
     public static $settings = array(
-        /*'mysql' => array(
+       /*
+        // Default DB connection
+        'mysql' => array(
             'server' => 'localhost',
             'database' => 'frawq',
             'username' => 'root',
             'password' => '',
-        ),*/
+        )
+        // To add mew settings for DB connection 
+        
+        'example' => array(
+            'server' => 'localhost',
+            'database' => 'exmple',
+            'username' => 'root',
+            'password' => '',
+        )*/
     );
    
 }
